a4c7cb0092316b0e54a99472936245b5dd279801
Sharyz <onshariiit@gmail.com>
2022-04-12 17:46:54 +0500

    category dynamic page

---
+++ pages/category/_slug.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="category-page"></div>
+</template>
+
+<script>
+export default {
+
+  name: "CategoryPage",
+
+  data () {
+    return {
+
+    }
+  }
+}
+</script>
+
+<style lang="css" scoped>
+</style>
